0680f804d85561eb53933ac416b8fd8170dcad34
vengatesh <karthickvengatesh001@gmail.com>
2025-08-28 22:18:46 +0530

    add tooltip variant

--- src/components/molecules/TooltipWrapper.tsx
+++ src/components/molecules/TooltipWrapper.tsx
@@ -1,10 +1,24 @@
 "use client"
 import { cn } from "@/utils/cn";
+import { cva, VariantProps } from "class-variance-authority";
 import { ReactNode } from "react";
 import { Tooltip, PlacesType } from "react-tooltip";
 import "react-tooltip/dist/react-tooltip.css";
 
-interface TooltipWrapperProps {
+const tooltipVariants = cva("!px-3 z-10 !py-2 relative !rounded-lg text-sm shadow-md", {
+  variants:{
+    variant: {
+      success: '!bg-green-200 [&>*]:text-emerald-700',
+      danger: '!bg-red-100 [&>*]:text-red-600',
+      info: "!bg-orange-400/30 [&>*]:text-yellow-800 border-orange-400/50",
+    }
+  },
+  defaultVariants: {
+    variant: 'success',
+  }
+})
+
+interface TooltipWrapperProps extends VariantProps<typeof tooltipVariants> {
   id: string;
   content: string | ReactNode;
   children: ReactNode;
@@ -17,6 +31,7 @@ export default function TooltipWrapper({
   content,
   children,
   className,
+  variant,
   place = "top",
 }: TooltipWrapperProps) {
   return (
@@ -25,7 +40,7 @@ export default function TooltipWrapper({
         {children}
       </span>
 
-      <Tooltip id={id} place={place} className={cn("!bg-green-200 !px-3 !py-2 !rounded-lg text-sm shadow-md", className)}>
+      <Tooltip id={id} place={place} className={cn(tooltipVariants({ variant }), className)}>
         {typeof content !== "string" ? content : null}
       </Tooltip>
     </>
--- src/components/organisms/RecipeInfoNutrientsCard.tsx
+++ src/components/organisms/RecipeInfoNutrientsCard.tsx
@@ -19,20 +19,21 @@ const nutritionChartViewRenderer = ({
   amount,
   percentOfDailyNeeds,
   unit,
-}: IRecipeNutritionDetails) => (
+}: IRecipeNutritionDetails, index:number) => (
   <div
     className="flex flex-col gap-4 justify-start items-center w-max p-2"
     key={name}
   >
-    <NutrientPieChart value={amount ?? 0} color="#ff8c8c" />
-    <div className="flex gap-4 justify-between items-center w-max  bg-red-200 text-red-800 border-red-200 rounded-lg px-4 py-1  text-center">
+    <NutrientPieChart value={amount ?? 0} color="#9f071290" />
+    <div className="flex gap-4 justify-between items-center w-max   text-red-800  rounded-lg px-4 py-1  text-center">
       <Typography level={6} className="font-medium text-red-800">
         {name} &nbsp; ({unit})
       </Typography>
       <TooltipWrapper
         id={`nutrition-vital-${name}`}
         className="ml-auto"
-        place="bottom"
+        place={(index + 1) % 4 === 0 ? "bottom-end" : "bottom"}
+        variant="info"
         content={
           <Typography level={6} className="text-sm font-normal">
             According to a recent survey a human body needs {percentOfDailyNeeds} ({unit}) 
@@ -40,7 +41,7 @@ const nutritionChartViewRenderer = ({
           </Typography>
         }
       >
-        <InformationCircleIcon className="h-6 !cursor-pointer w-6 text-black/75 ml-auto" />
+        <InformationCircleIcon className="h-6 !cursor-pointer w-6  ml-auto" />
       </TooltipWrapper>
     </div>
   </div>
@@ -71,9 +72,9 @@ function RecipeInfoNutrientsCard({
         items={nutrients}
         offset={4}
         itemWrapperClassName="grid grid-cols sm:grid-cols-2 md:grid-cols-4 place-items-center place-content-center gap-4 py-4"
-        prevPageButtonClassName="border-red-300 text-inherit hover:bg-red-300 hover:text- hover:[&>.left-arrow]:text-white [&>.left-arrow]:text-red-500"
-        nextPageButtonClassName="border-red-300 text-inherit hover:bg-red-300 hover:text-white  hover:[&>.right-arrow]:text-white [&>.right-arrow]:text-red-500"
-        displayPageNumberClassName="text-red-800 [&>.current-page]:text-red-500"
+        prevPageButtonClassName="border-red-800 text-inherit hover:bg-red-800 hover:text- hover:[&>.left-arrow]:text-white [&>.left-arrow]:text-red-500"
+        nextPageButtonClassName="border-red-800 text-inherit hover:bg-red-800 hover:text-white  hover:[&>.right-arrow]:text-white [&>.right-arrow]:text-red-500"
+        displayPageNumberClassName="text-[#9f071285] [&>.current-page]:text-red-800"
         render={nutritionChartViewRenderer}
       />
     </motion.div>
